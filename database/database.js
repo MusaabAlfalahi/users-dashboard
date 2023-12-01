cf4c6e715955bf80b7fa9943c8dd0294653fed4d
MusaabAlfalahi <mosaab.dulimi@yahoo.com>
2023-12-01 18:49:27 +0300

    Revert "deploy"
    This reverts commit 45010c899fbce628dfcfafb3c86e157edc964392.

--- database/database.js
+++ database/database.js
@@ -7,9 +7,9 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 
-const sequelize = new Sequelize(defaultdb, avnadmin, AVNS_g3lg5lWkKtO8Qh_8V6A, {
-  host: mysql-users-dashboard-mosaab.a.aivencloud.com,
-  port: 12251,
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
   dialect: 'mysql',
 });
 
