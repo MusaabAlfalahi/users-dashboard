f309145dc47a0a6f838ecfe5b4896b79141043b0
musaab al-falahi <alfalahimusaab@gmail.com>
2023-12-01 18:37:07 +0300

    deploy

--- database/database.js
+++ database/database.js
@@ -1,15 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const DB_NAME = process.env.DB_NAME;
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
-
-const sequelize = new Sequelize(defaultdb, avnadmin, AVNS_g3lg5lWkKtO8Qh_8V6A, {
-  host: mysql-users-dashboard-mosaab.a.aivencloud.com,
-  port: 12251,
+const sequelize = new Sequelize("defaultdb", "avnadmin", "AVNS_g3lg5lWkKtO8Qh_8V6A", {
+  host: "mysql-users-dashboard-mosaab.a.aivencloud.com",
+  port: "12251",
   dialect: 'mysql',
 });
 
